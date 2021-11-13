1d0b80cc3ab7b705200b33a3655e196ddc7b8904
akash <akashlil393@gmail.com>
2021-11-13 19:00:55 +0600

    add env

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -1,10 +1,10 @@
 const firebaseConfig = {
-  apiKey: "AIzaSyCwOK5YdM6gSObO6nOu-El0RosivJndv2c",
-  authDomain: "race-zone-ce537.firebaseapp.com",
-  projectId: "race-zone-ce537",
-  storageBucket: "race-zone-ce537.appspot.com",
-  messagingSenderId: "859251361721",
-  appId: "1:859251361721:web:c0e307b88211d811f59868",
+  apiKey: process.env.REACT_APP_apiKey,
+  authDomain: process.env.REACT_APP_authDomain,
+  projectId: process.env.REACT_APP_projectId,
+  storageBucket: process.env.REACT_APP_storageBucket,
+  messagingSenderId: process.env.REACT_APP_messagingSenderId,
+  appId: process.env.REACT_APP_appId,
 };
 
 export default firebaseConfig;
